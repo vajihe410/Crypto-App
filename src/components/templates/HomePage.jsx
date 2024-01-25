bf422d7312df7951ecb4765dc3f7a00d1a877f81
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-01-25 20:38:27 +0330

    pagination with styles

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from 'react'
+//components
 import TableCoin from '../modules/TableCoin'
+import Pagination from '../modules/Pagination.jsx';
+
 import { getCoinList } from '../../services/cryptoApi.js';
 
 function HomePage() {
 
-
     const [coins,setCoins] = useState([]);
     const [isLoading,setIsLoading] = useState(true)
+    const [page,setPage] = useState(1)
 
     useEffect( () => {
+        setIsLoading(true)
         const getData =async () => {
-            const response = await fetch(getCoinList())
+            const response = await fetch(getCoinList(page))
             const json = await response.json()
             setCoins(json)
             setIsLoading(false)
         }
         getData()
-    } ,[])
+    } ,[page])
 
   return (
     <div>
         <TableCoin coins={coins} isLoading={isLoading}/>
+        <Pagination page={page} setPage={setPage}/>
     </div>
   )
 }
