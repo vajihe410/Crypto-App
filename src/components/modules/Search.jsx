448bcf4e1bfa3420843c3662b5c3827e7374d171
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-02-01 13:07:58 +0330

    Style: add styles of search box and search result

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { getSearchCoin } from '../../services/cryptoApi'
 //spinner
 import { RotatingLines } from 'react-loader-spinner'
+//styles
+import styles from './Search.module.css'
 
 function Search({ currency,setCurrency }) {
 
@@ -41,24 +43,25 @@ function Search({ currency,setCurrency }) {
         return () => controller.abort()
     },[text])
   return (
-    <div>
+    <div className={styles.searchBox}>
         <input type='text' placeholder='Search' value={text} onChange={e => setText(e.target.value) }/>
         <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
             <option value="usd">USD</option>
             <option value="eur">EUR</option>
             <option value="jpy">JPY</option>
         </select>
-        <div>
-          {isLoading ? <RotatingLines width='50px' height='50px' strokeWidth="2" strokeColor="#3874ff"/>:(
-            <ul>
-               {coins.map(coin => <li key={coin.id}>
-                                    <img src={coin.thumb} alt={coin.name}/>
-                                    <p>{coin.name}</p>
-                                  </li>)}
-            </ul>
+    {(!!coins.length || isLoading) && (
+      <div className={styles.searchResult}>
+        {isLoading ? <RotatingLines width='50px' height='50px' strokeWidth="2" strokeColor="#3874ff"/>:(
+          <ul>
+            {coins.map(coin => <li key={coin.id}>
+                                <img src={coin.thumb} alt={coin.name}/>
+                                <p>{coin.name}</p>
+                                </li>)}
+          </ul>
           )}
         
-        </div>
+      </div>)}
     </div>
   )
 }
